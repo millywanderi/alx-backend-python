9234fa18d2dfb74b2fa2fe00d3a9801890553191
millywanderi <wanderinyambura5@gmail.com>
2024-01-16 14:27:14 +0300

    A coroutine called async_comprehension that takes no arguments.

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,13 +2,12 @@
 """
 A coroutine called async_comprehension that takes no arguments.
 """
-import asyncio
-import random
-from typing import Generator
+
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """Async Comprehension"""
     return [i async for i in async_generator()]
