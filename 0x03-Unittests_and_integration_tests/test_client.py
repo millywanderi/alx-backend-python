04cbc74946ebb8c597c65aa0b1c9a8b5371410d9
millywanderi <wanderinyambura5@gmail.com>
2024-02-01 13:47:20 +0300

    test that GithubOrgClient.org returns the correct value

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Module for testing client"""
+from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
+from parameterized import parameterized, parameterized_class
+import json
+import unittest
+from unittest.mock import patch, PropertyMock, Mock
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Class for testing Github Org Client"""
+
+    @parameterized.expand([
+        ('google'),
+        ('abc')
+    ])
+    @patch('client.get_json')
+    def test_org(self, input, mock):
+        """Test for Github Org Client"""
+        test_class = GithubOrgClient(input)
+        test_class.org()
+        mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
