52e36d519588788ff3a626141d95f67f5bb91c77
millywanderi <wanderinyambura5@gmail.com>
2024-02-01 12:39:27 +0300

    Implement the TestAccessNestedMap.test_access_nested_map method

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+A module that parameterize a unit test
+"""
+import unittest
+from parameterized import parameterized
+from unittest.mock import Mock, patch
+from typing import Dict, Tuple, Union
+
+from utils import (
+    access_nested_map,
+    get_jason,
+    memoize,
+)
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Tests `access_nested_map` function."""
+    @paramitized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            expected_result: Union[Dict, int],
+            ) -> None:
+        """A method that parameterize a unit test"""
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
