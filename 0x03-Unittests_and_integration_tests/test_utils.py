c15cd62ec3ba94eca850c3b4ba438cd4719e334f
millywanderi <wanderinyambura5@gmail.com>
2024-02-01 12:56:50 +0300

    Tests  exception raising

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,17 +6,12 @@ import unittest
 from parameterized import parameterized
 from unittest.mock import Mock, patch
 from typing import Dict, Tuple, Union
-
-from utils import (
-    access_nested_map,
-    get_jason,
-    memoize,
-)
+from utils import access_nested_map, get_jason, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """Tests `access_nested_map` function."""
-    @paramitized.expand([
+    @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
@@ -29,3 +24,20 @@ class TestAccessNestedMap(unittest.TestCase):
             ) -> None:
         """A method that parameterize a unit test"""
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",), keyError),
+        ({"a": 1}, ("a", "b"), keyError),
+    ])
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            exception: Exception,
+            ) -> None:
+    """Tests `test_access_nested_map` exception raising"""
+    with self.assertRaises(exception):
+        access_nested_map(nested_map, path)
+
+
+
